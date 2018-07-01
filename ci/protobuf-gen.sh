653ed6f634403ce19a42e2cd41ea2f17a2394ccd
cryptocode <cryptocode@zolo.io>
2018-07-02 00:46:55 +0200

    Added support for util.proto

--- ci/protobuf-gen.sh
+++ ci/protobuf-gen.sh
@@ -1,10 +1,9 @@
 # Download latest protobuf definition
-rm master.tar.gz
-wget --no-check-certificate https://github.com/nanoapi/protobuf/archive/master.tar.gz
-tar xpvf master.tar.gz
+mkdir -p protobuf-master/google/protobuf
+wget --quiet --no-check-certificate https://raw.githubusercontent.com/cryptocode/raiblocks/c-api/protobuf/core.proto -O protobuf-master/core.proto
+wget --quiet --no-check-certificate https://raw.githubusercontent.com/cryptocode/raiblocks/c-api/protobuf/util.proto -O protobuf-master/util.proto
+wget --quiet --no-check-certificate https://raw.githubusercontent.com/cryptocode/raiblocks/c-api/protobuf/google/protobuf/wrappers.proto -O protobuf-master/google/protobuf/wrappers.proto
 
 # Generate Java files
 protoc --proto_path=protobuf-master --go_out=src/nano_api protobuf-master/core.proto
-
-# Cleanup
-rm master.tar.gz
+protoc --proto_path=protobuf-master --go_out=src/nano_api protobuf-master/util.proto
